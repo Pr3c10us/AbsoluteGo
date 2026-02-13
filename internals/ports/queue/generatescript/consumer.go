9f2704b50d64c9b83094fff1dbfcdd4072741e9c
Owolabi precious <owo.pre.eno@gmail.com>
2026-02-13 02:16:09 +0100

    feat: Add script management module with dedicated services, HTTP handlers, and queue consumers for generation.

---
+++ internals/ports/queue/generatescript/consumer.go
@@ -0,0 +1,17 @@
+package addchapter
+
+import (
+	"encoding/json"
+	"github.com/Pr3c10us/absolutego/internals/domains/queueport"
+	"github.com/Pr3c10us/absolutego/internals/services/book/commands"
+)
+
+func Handler(c *queueport.Context) error {
+	var data commands.AddChapterParameter
+	err := json.Unmarshal(c.Data, &data)
+	if err != nil {
+		return err
+	}
+	err = c.Services.BookServices.AddChapter.Handle(data)
+	return err
+}
